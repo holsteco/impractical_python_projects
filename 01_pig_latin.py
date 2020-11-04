bdde1b3df1338e1c65da37e9f5e33944e1373f33
Corey Holstege <corey.holstege@gmail.com>
2020-11-04 02:30:52 +0000

    01_pig_latin created and started

---
+++ 01_pig_latin.py
@@ -0,0 +1,10 @@
+#take word as input
+#use indexing to slice and reutn it's pig latin equivalent
+##if first letter of the word is a consonant, move consonant to end of word, add "ay" to end
+##if first letter of the word is a vowel, add "way" to the print
+
+"""Turn a word into its Pig Latin equivalent """
+import sys
+
+vowels = 'aeiouy'
+
